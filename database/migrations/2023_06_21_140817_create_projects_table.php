cc9405753f6ce961dc3bb23e1c7361c243ee04cc
valeriofilareti <valerio.filareti@gmail.com>
2023-06-21 16:53:56 +0200

    seed table

--- database/migrations/2023_06_21_140817_create_projects_table.php
+++ database/migrations/2023_06_21_140817_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->date('creation_date');
             $table->timestamps();
         });
     }
